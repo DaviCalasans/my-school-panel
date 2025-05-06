4540b0a70d0af1f37398cbbdd2a19337aa7d5982
Davi Calasans <davivida878@gmail.com>
2025-05-05 23:11:05 -0300

    feat: Alterar o input de gender para radio

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 function myEscope(){
     const users = [];
     const form = document.querySelector('.form')
-    const name = form.querySelector('#name')
-    const age = form.querySelector('#age')
-    const gender = form.querySelector('#gender')
-    
-    function getInfoUsers(event){
+
+    function getFormValues(event){
         event.preventDefault();
+        const name = form.querySelector('#name')
+        const age = form.querySelector('#age')
+        const gender = form.querySelector('input[name="gender"]:checked')
+        
         let nameValue = name.value;
         let ageValue = age.value;
         let genderValue = gender.value;
         
         const newUser = createUser(nameValue,ageValue,genderValue);
+        createCard();
         renderUsertoScreen(newUser)
 
         form.reset();
 
     }
-
+    
+    function createUser(name, age, gender){
+        return{
+            name: name,
+            age: age,
+            gender: gender
+            
+        }
+    }
+    
+    function createCard(){
+        const containerCard = document.querySelector('.container-card-user')
+        const cardUserlayout = document.createElement('div');
+        cardUserlayout.className = 'card-user'
+        containerCard.appendChild(cardUserlayout);
+    }
+    
     function renderUsertoScreen(newUser){
         const cardUser = document.querySelectorAll('.card-user');
     
@@ -25,19 +43,13 @@ function myEscope(){
         
                 for(let i = 0; i <= users.length - 1;i++){
                     const {name, age, gender} = users[i];
-                    cardUser[i].innerHTML = `${name} ${age} ${gender}`;
+                    cardUser[i].innerHTML = `<p>${name}</p> <p>${age}</p> <p>${gender}</p>`;
                 }
     };
     
-    function createUser(name, age, gender){
-        return{
-            name: name,
-            age: age,
-            gender: gender
-    
-        }
-    }
-    form.addEventListener('submit',getInfoUsers)
+
+
+    form.addEventListener('submit',getFormValues)
     console.log(users)
     
     
