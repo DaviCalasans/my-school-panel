702bc48c65b2c43718241c4142ca366e0733935c
Davi Calasans <davivida878@gmail.com>
2025-07-07 16:22:35 -0300

    feat(form): adicionar novos campos ao formulário de cadastro

--- script.js
+++ script.js
@@ -6,29 +6,73 @@ function myEscope(){
 
     function getFormValues(event){
         event.preventDefault();
-        const name = form.querySelector('#name')
-        const age = form.querySelector('#age')
-        const gender = form.querySelector('input[name="gender"]:checked')
+        const name = form.querySelector('#name');
+        const age = form.querySelector('#age');
+        const gender = form.querySelector('input[name="gender"]:checked');
+        const registration = form.querySelector('#matricula');
+        const serie = form.querySelector("#series");
         
         let nameValue = name.value;
-        let ageValue = age.value;
-        let genderValue = gender.value;
-        
-        const newUser = createUser(nameValue,ageValue,genderValue);
+        let formatAgeValue = age.value; //Pega o valor da idade passado no input
+        let genderValue = getIconGender(gender.value);
+        console.log(genderValue)
+        let ageValue = formatData(formatAgeValue); //Formata a data para o dd//mm/aaaa
+        let registrationValue = parseInt(registration.value); //Pega o valor da matrícula e transforma em tipo number
+        let serieValue = serie.value;
+        console.log(ageValue, serieValue);
+
+        const newUser = createUser(nameValue,ageValue,genderValue,registrationValue, serieValue);
         createCard();
         renderUsertoScreen(newUser);
+        console.log(typeof ageValue, ageValue);
         layoutForm.style.display = 'none';
 
         form.reset();
 
     }
+
+    function getIconGender(genderValue){
+        if(genderValue === 'Masculino'){
+            return {
+                textGender: 'Masculino',
+                iconGender: 'imgs/male-icon.png'
+            }
+        }else{
+            return {
+                textGender: 'Feminino',
+                iconGender: 'imgs/female-icon.png'
+            }
+        }
+    }
+
+    //Função para formatar data e calcular a idade do usuário
+    function formatData(formatAgeValue){
+        const [ano, mes, dia] = formatAgeValue.split('-'); //Desestruturção da string data
+        const dateBorn = new Date(ano, mes - 1, dia); //Criação de data com os valores desestruturados
+        const somentedate = dateBorn.toLocaleDateString() //Formata a data de nascimento para mostrar somente a data no formato dd/mm/aaaa
+        const currentDate = new Date().toLocaleDateString(); //Pega a data atual em formato de dd/mm/aaaa
+        const parts = currentDate.split('/'); 
+        const currentYear = parts[2];//Pega a última parte (ano) da string data
+
+        const currentAge = currentYear - ano; //Faz o cálculo para encontrar a idade do usuário
+
+        //Retorna um objeto com data de nascimento e idade do usuário
+        return {
+            dataNascimento: somentedate,
+            idade: currentAge
+        }
+    };
     
-    function createUser(name, age, gender){
+    //Agora a factory recebe um objeto na idade (data de nascimento e idade)
+    function createUser(name, ageObj, genderObj, registrationValue, serieValue){
         return{
             name: name,
-            age: age,
-            gender: gender
-            
+            dateBorn: ageObj.dataNascimento,
+            age: ageObj.idade,
+            textGender: genderObj.textGender,
+            iconGender: genderObj.iconGender,
+            registrationValue: registrationValue,
+            serieValue: serieValue
         }
     }
     
@@ -41,17 +85,17 @@ function myEscope(){
     
     function renderUsertoScreen(newUser){
         const cardUser = document.querySelectorAll('.card-user');
-        console.log(cardUser);
-        console.log(users);
-    
+        console.log(users)
         users.push(newUser);
         
                 for(let i = 0; i <= users.length - 1;i++){
-                    const {name, age, gender} = users[i];
+                    const {name, dateBorn, age, textGender,iconGender, registrationValue, serieValue} = users[i]; // Desestrutura com a data de nascimento
                     cardUser[i].innerHTML = `
                     <p>${name}</p>
-                    <p>${age}</p> 
-                    <p>${gender}</p>
+                    <p>${dateBorn} (${age} anos)</p> 
+                    <p>Matrícula: 0000${registrationValue}</p> 
+                    <p>${serieValue}</p> 
+                    <p>${textGender} <img src="${iconGender}" id="male-icon"></p>
                     <button onClick="deleteUser(${i})">Deletar</button>
                     `;
                 }
@@ -91,6 +135,7 @@ function myEscope(){
             `;
         }
     }
+
     
     form.addEventListener('submit',getFormValues)
     
